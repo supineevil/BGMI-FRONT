88b02acbdf2583c66b781f929fffe8acfb22b5e1
Raunak Ghosh <75155924+supineevil@users.noreply.github.com>
2023-08-12 11:58:44 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  theme: {
+    fontFamily: {
+      'bgmi': ['Fjalla One', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
+
